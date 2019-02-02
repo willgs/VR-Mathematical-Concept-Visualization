7f8b213dde6055a5b90e16278e365ba55c13b7f0
danliszka <danliszka1@gmail.com>
2019-02-02 13:24:24 -0500

    created function to extract coordinates from a figure file and presents it in a nice format. Currently only works with 3D surfaces

---
+++ Code/convert.m
@@ -0,0 +1,6 @@
+function convert(inputFigureFileName,outputLocation)
+%This function will be the parent converter, utilizing other functions and
+%   its own algorithms to convert a figure file to an object file
+
+end
+
---
+++ Code/extractCoordinates.m
@@ -0,0 +1,35 @@
+function coordinateMatrix = extractCoordinates(inputFigureFile)
+%This will take the input figure file and extract the x-y-z coordinates
+%   from the children structures of the figure file
+    
+%   read data into workspace 
+fig = openfig(inputFigureFile,'invisible');
+    
+%   extract data from figure and get type --fix for 2D surfaces
+d = fig.Children.Children;
+
+switch(d.Type)
+    case 'surface'
+        
+        [row,col] = size(d.XData);
+        len = row*col;
+        
+        coordinateMatrix = zeros(len,3);
+        
+        %merge data into one coordinate matrix
+        k = 1;
+        for i=1:row
+            for j=1:col
+                coordinateMatrix(k,1) = d.XData(i,j);
+                coordinateMatrix(k,2) = d.YData(i,j);
+                coordinateMatrix(k,3) = d.ZData(i,j);
+                k=k+1;
+            end
+        end
+        
+    case 'line'
+        
+
+
+end
+
